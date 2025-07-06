f9bd1dc601a4b7937b13d255e9a2489f24864516
sampasj <sampa@teamsampa.com>
2025-07-06 20:28:20 +0900

    modify asm6502

---
+++ test1.asm
@@ -0,0 +1,19 @@
+     .ORG    $8000
+     NOP     ; No OPeration
+     NOP
+     ADC $8000
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
+     NOP
