666b05d3445888f6720c13544d105c429c97cac3
sampa <sampa@teamsampa.com>
2026-06-21 19:52:54 +0900

    modify asm6502

--- test1.asm
+++ test1.asm
@@ -2,16 +2,20 @@
      NOP     ; No OPeration
      NOP
      ADC $0404
+     TAX
      PHA
      ADC $0400
+     TXS
      PHA
      ADC $0404
      PLA
      TAX
+     TAY
      PLA
      PHP
      SEC
      PLP
-     SED
+     TSX
+     TXA
      NOP
      NOP
