c4c40aec5bcfe6fac5ce5cad6b25a7c6cd4e335b
sampa <sampa@teamsampa.com>
2026-07-27 19:35:54 +0900

    modify asm6502

--- test1.asm
+++ test1.asm
@@ -1,19 +1,19 @@
      .ORG    $400
      NOP     ; No OPeration
      LDA #$11
+     SEC
+     ADC $30
+     ADC #$05
+     ADC $0400,X
+     ADC $0400,Y
+     ADC $30,X
      TAX
      LDA ($A1),Y
      LDA ($A1,X)
      LDA $0400,Y
      TAX
      PHA
-     ADC $0400
      TXS
      PHA
      ADC $0404
      PLA
-     TAX
-     TAY
-     TYA
-     NOP
-     NOP
